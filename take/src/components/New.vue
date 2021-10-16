73db4730d9412511ab8dcaa5d7ea329dbd1bb9c7
yyh <3250007027@qq.com>
2021-10-16 14:23:52 +0800

    首页大分类搜索

---
+++ take/src/components/New.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{bigId}}
+    </div>
+</template>
+<script>
+export default {
+   props:['bigId'] 
+}
+</script>
+
+<style scoped>
+
+</style>
--- take/src/router/index.js
+++ take/src/router/index.js
@@ -7,6 +7,7 @@ import My from '@/components/My.vue';
 import Login from '@/components/Login.vue';
 import Register from '@/components/Register.vue'
 import List from '@/components/List.vue'
+import New from '@/components/New.vue'
 
 
 
@@ -20,10 +21,11 @@ export default new Router({
         },
         { path: '/index', component: Index },
         { path: '/order', component: Order },
-        { path: '/search/:bigId', component: Search,props:true},
+        { path: '/search/', component: Search,props:true},
         { path: '/my', component: My },
         { path: '/login', component: Login },
         { path: '/register', component: Register },
+        { path: '/new/:bigId', component: New,props:true },
         { path: '/list/:id', component: List ,props:true}
     ]
 })
